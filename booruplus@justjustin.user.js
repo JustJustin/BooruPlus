0f1a9c1e439907cbde1cabf51fceb6f846e19f7f
Justin <justkiri@magicalgirlnoir.com>
2012-10-08 03:37:25 -0700

    Expanded Pool Browsing
    Made Pool Browsing much more robust, also added easy binds for going to
    the next and previous page.

--- booruplus@justjustin.user.js
+++ booruplus@justjustin.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @id             BooruPlus@JustJustin
 // @name           BooruPlus
-// @version        .01
+// @version        .1
 // @namespace      JustJustin.BooruPlus
 // @author         JustJustin
-// @description    BooruPlus
+// @description    Provides key based pool navigation, url tag removal, and other shortcuts.
 // @include        http://danbooru.donmai.us/*
 // @include        https://danbooru.donmai.us/*
 // @run-at         document-end
@@ -107,8 +107,53 @@ var BooruPlus = {};
 $base.extend(BooruPlus, {
     init: function() {
         this.urlShorten();
-        this.keyBind();
+		this.poolBrowse.init();
+		this.paginationBind.init();
     },
+	paginationBind: {
+		nextURL: null,
+		prevURL: null,
+		init: function() {
+			//Add binds to << and >> page buttons if they exist.
+			var pagereg = /\/post\/index|\/pool\/index|\/pool\/show\//;
+			if(pagereg.exec(location.pathname)) {
+				console.log("Pagination Page");
+				var links = $$base('.pagination a');
+				for(var i = 0; i < links.length; ++i) {
+					if(links[i].innerHTML == '&lt;&lt;') {
+						this.prevURL = links[i].href;
+					}
+					if(links[i].innerHTML == '&gt;&gt;') {
+						this.nextURL = links[i].href;
+					}
+				}
+				
+				document.addEventListener('keydown', this.keyHandler);
+			}
+		},
+		keyHandler: function(e) {
+			switch(e.keyCode) {
+				case 39:
+					BooruPlus.paginationBind.next();
+					break;
+				case 37:
+					BooruPlus.paginationBind.prev();
+					break;
+				default:
+					break;
+			}
+		},
+		next: function() {
+			if(this.nextURL) {
+				window.location = this.nextURL;
+			}
+		},
+		prev: function() {
+			if(this.prevURL) {
+				window.location = this.prevURL;
+			}
+		}
+	},
     urlShorten: function() {
      var reg = /post\/show\//;
      var url = window.location.pathname;
@@ -122,24 +167,123 @@ $base.extend(BooruPlus, {
         }
      }
     },
-    keyBind: function() {
-		var els = $$base('.status-notice a');
-		var el = false;
-		for(var i = 0; i < els.length; ++i) { 
-			if(els[i].innerHTML == '&gt;&gt;') {
-				el = els[i];
-				break;
+	poolBrowse: {
+		currentPool: null,
+		init: function() {
+			var poolReg = /\/pool\/show\//;
+			
+			//Detect if we are in a pool
+			if(poolReg.exec(location.pathname)) {
+				this.poolPage();
 			}
-		};
-		if(el)
-		{
-			document.addEventListener('keydown', function(e) { 
-				if(e.keyCode == 39) {
-					location.href = el.href;
+			
+			//Detect if we are viewing a image
+			else if(/post\/show\//.exec(location.pathname)){
+				this.poolImage();
+				document.addEventListener('keydown', this.keyHandler);
+			}
+		},
+		poolPage: function() {
+			var id = parseInt(location.pathname.substr(location.pathname.lastIndexOf('/')+1));
+			console.log("Pool " + id);
+			this.currentPool = id;
+			
+			var links = $$base('#pool-show .thumb a');
+			for(var i = 0; i < links.length; ++i) {
+				links[i].addEventListener('click', function() {
+					BooruPlus.poolBrowse.savePool();
+					return true;
+				});
+			}
+		},
+		poolImage: function() {
+			//Detect if we are viewing a pool post
+			var pools = [];
+			var els = $$base('.status-notice');
+			for(var i = 0; i < els.length; ++i) {
+				if(els[i].id.search('pool') != -1) {
+					pools.push(els[i]);
+					//Attach click event handlers only happens for pools
+					var links = $$base('a', els[i]);
+					for(var n = 0; n < links.length; ++n) {
+						links[n].addEventListener('click', this.browseClick);
+					}
 				}
-			});
+			}
+			//No Pools found.
+			if(pools.length == 0) {
+				return;
+			}
+			console.log('Pools Image');
+			console.log(pools);
+			
+			//Set currentPool based on localStorage if it is present 
+			if(localStorage.getItem('browsePool')) {
+				try{
+					this.currentPool = JSON.parse( localStorage.getItem('browsePool') );
+					if($base('#pool' + this.currentPool)) {
+						console.log("Loaded saved Pool " + this.currentPool);
+						return;
+					}
+				}
+				catch(e){}
+			}
+			//Set currentPool to first pool availabe
+			this.currentPool = this.getID(pools[0]);
+			console.log("Selected Pool " + this.currentPool);
+		},
+		browseClick: function(e) {
+			if(this.href.search('post') != -1) {
+				BooruPlus.poolBrowse.savePool(BooruPlus.poolBrowse.getID(this.parentNode));
+			}
+		},
+		getID: function(pool) {
+			return parseInt(pool.id.substring(4));
+		},
+		savePool: function(poolID) {
+			if(poolID != undefined) {
+				this.currentPool = poolID;
+			}
+			localStorage.setItem('browsePool', JSON.stringify(this.currentPool));
+			console.log("Saved " + this.currentPool);
+		},
+		keyHandler: function(e) {
+			switch(e.keyCode) {
+				case 39: 
+					BooruPlus.poolBrowse.next();
+					break;
+				case 37:
+					BooruPlus.poolBrowse.prev();
+					break;
+				default:
+					break;
+			}
+		},
+		next: function() {
+			if(!this.currentPool) {
+				return false;
+			}
+			var els = $$base('#pool' + this.currentPool + ' a');
+			for(var i = 0; i < els.length; ++i) {
+				if(els[i].innerHTML == '&gt;&gt;') {
+					this.savePool();
+					window.location = els[i].href;
+				}
+			}
+		},
+		prev: function() {
+			if(!this.currentPool) {
+				return false;
+			}
+			var els = $$base('#pool' + this.currentPool + ' a');
+			for(var i = 0; i < els.length; ++i) {
+				if(els[i].innerHTML == '&lt;&lt;') {
+					this.savePool();
+					window.location = els[i].href;
+				}
+			}
 		}
-    }
+	}
 });
 
 BooruPlus.init();
